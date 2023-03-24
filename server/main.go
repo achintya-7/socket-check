bde25656c71dfe1d399c586c0ba106618620df15
achintya <achintya22052000@gmail.com>
2023-03-24 06:47:53 +0000

    first commit

---
+++ server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	socketio "github.com/googollee/go-socket.io"
+)
+
+func main() {
+	router := gin.New()
+
+	server := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		log.Println("connected:", s.ID())
+		return nil
+	})
+
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		log.Println("closed", reason)
+	})
+
+	go func() {
+		if err := server.Serve(); err != nil {
+			log.Fatalf("socketio listen error: %s\n", err)
+		}
+	}()
+	defer server.Close()
+	
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, "Pong\n")
+	})
+	router.GET("/socket.io/*any", gin.WrapH(server))
+	router.POST("/socket.io/*any", gin.WrapH(server))
+
+	if err := router.Run(":8000"); err != nil {
+		log.Fatal("failed run app: ", err)
+	}
+}
